5205a06ccc4ede385960303141402edec5fb2f55
gohdg <gohdg@naver.com>
2024-09-11 15:52:51 +0900

    Pydantic, Model Class, Nested Model

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 from fastapi import FastAPI, HTTPException, status
-from enum import Enum
-
+from schemas import GenreURLChoices, Band
 
 app = FastAPI()
-
-
-class GenreURLChoices(Enum):
-    ROCK = "rock"
-    ELECTRONIC = "electronic"
-    METAL = "metal"
-    HIP_HOP = "hip-hop"
-
 
 BANDS = [
     {"id": 1, "name": "The kinks", "genre": "Rock"},
     {"id": 2, "name": "Aphex Twin", "genre": "Electronic"},
-    {"id": 3, "name": "Black Sabbath", "genre": "Metal"},
+    {"id": 3, "name": "Black Sabbath", "genre": "Metal", "album": [
+        {"title": "Master of Reality", "release_date": "1979-07-21"},
+        {"title": "Master of Reality", "release_date": "1979-07-21"}
+    ]},
     {"id": 4, "name": "Wu-Tang clan", "genre": "Hip-Hop"},
 ]
 
 
 @app.get("/bands")
-async def bands() -> list[dict]:
-    return BANDS
+async def bands() -> list[Band]:
+    # Band(**b)로 초기값 세팅이 가능한 이유는 BaseModel를 상속받기 때문이다.
+    # Pydantic이 자동으로 __init__() 메서드를 생성
+    # 1.	Pydantic의 BaseModel:
+    # Pydantic의 BaseModel을 상속받는 클래스는 자동으로 데이터 검증 및 초기화를 위한 __init__() 메서드를 제공합니다.
+    # Pydantic 모델은 클래스 속성을 필드로 사용하며, 이 필드들은 모델 인스턴스를 생성할 때 초기화되고 인스턴스 변수로 변경됩니다.
+    return [
+        Band(**b) for b in BANDS
+    ]
 
 
 @app.get("/bands/{band_id}")
-async def band(band_id: int) -> dict:
-    band = next((band for band in BANDS if band['id'] == band_id), None)
+async def band(band_id: int) -> Band:
+    band = next((Band(**band)
+                for band in BANDS if band['id'] == band_id), None)
 
     if band is None:
         raise HTTPException(
---
+++ schemas.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from datetime import date
+from pydantic import BaseModel
+
+
+class GenreURLChoices(Enum):
+    ROCK = "rock"
+    ELECTRONIC = "electronic"
+    METAL = "metal"
+    HIP_HOP = "hip-hop"
+
+
+class Album(BaseModel):
+    title: str
+    release_date: date
+
+
+class Band(BaseModel):
+    id: int
+    name: str
+    genre: str
+    album: list[Album] = []
